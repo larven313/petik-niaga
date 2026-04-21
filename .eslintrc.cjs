8603a1d04270ad51534adbd29dac2a4866691047
Sukma Hidayatullah <hidayatullahsukma@gmail.com>
2026-04-21 16:24:38 +0700

    mengubah aturan .eslint supaya variable tidak dipake ga ada wanring

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,8 +10,9 @@ module.exports = {
   settings: { react: { version: "18.2" } },
   plugins: ["react-refresh"],
   rules: {
-    "react-refresh/only-export-components": "warn",
     "react/prop-types": "off",
     "no-unused-vars": "off",
+    "react-hooks/exhaustive-deps": "off", // ← matikan warning useEffect
+    "react-refresh/only-export-components": "off", // ← matikan warning DashboardContext
   },
 };
